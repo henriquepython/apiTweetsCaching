d4169d52c35f9fc14c4d1b798c04118acd2044e9
Henriquepython <joaohbp59@gmail.com>
2022-04-13 16:35:07 -0300

    finish

--- src/tweets/tweets.controller.ts
+++ src/tweets/tweets.controller.ts
@@ -9,7 +9,6 @@ import {
 } from '@nestjs/common';
 import { TweetsService } from './tweets.service';
 import { CreateTweetDto } from './dto/create-tweet.dto';
-import { UpdateTweetDto } from './dto/update-tweet.dto';
 
 @Controller('tweets')
 export class TweetsController {
@@ -23,20 +22,5 @@ export class TweetsController {
   @Get()
   findAll() {
     return this.tweetsService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.tweetsService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateTweetDto: UpdateTweetDto) {
-    return this.tweetsService.update(+id, updateTweetDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.tweetsService.remove(+id);
   }
 }
--- src/tweets/tweets.service.ts
+++ src/tweets/tweets.service.ts
@@ -15,18 +15,6 @@ export class TweetsService {
   }
 
   findAll() {
-    return `This action returns all tweets`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} tweet`;
-  }
-
-  update(id: number, updateTweetDto: UpdateTweetDto) {
-    return `This action updates a #${id} tweet`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} tweet`;
+    return this.tweetModel.findAll();
   }
 }
